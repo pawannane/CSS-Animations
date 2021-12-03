c3e51b68dbbb619a2a4384242ca56adf093f07b9
pawannane <pawannane23@gmail.com>
2021-12-03 12:13:20 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,72 @@
+//Menu-toggle button
+$(document).ready(function () {
+    $(".menu-icon").on("click", function () {
+        $("nav ul").toggleClass("showing");
+    });
+});
+
+//Scrolling Effect
+$(window).on("scroll", function () {
+    if ($(window).scrollTop()) {
+        $('nav').addClass('black');
+    } else {
+        $('nav').removeClass('black');
+    }
+});
+
+// grid images animation
+//Creating 50 thumbnails inside .grid
+//the images are stored on the server serially. So we can use a loop to generate the HTML.
+var images = "",
+    count = 50;
+for (var i = 1; i <= count; i++)
+    images += '<img src="http://thecodeplayer.com/u/uifaces/' + i + '.jpg" />';
+
+//appending the images to .grid
+$(".grid").append(images);
+
+var d = 0; //delay
+var ry, tz, s; //transform params
+
+//animation time
+$(".animate").on("mouseenter", function () {
+    //fading out the thumbnails with style
+    $(".animation img").each(function () {
+        d = Math.random() * 1000; //1ms to 1000ms delay
+        $(this).delay(d).animate({
+            opacity: 0
+        }, {
+            //while the thumbnails are fading out, we will use the step function to apply some transforms. variable n will give the current opacity in the animation.
+            step: function (n) {
+                s = 1 - n; //scale - will animate from 0 to 1
+                $(this).css("transform", "scale(" + s + ")");
+            },
+            duration: 1000,
+        })
+    }).promise().done(function () {
+        //after *promising* and *doing* the fadeout animation we will bring the images back
+        storm();
+    })
+})
+
+//bringing back the images with style
+function storm() {
+    $(".animation img").each(function () {
+        d = Math.random() * 1000;
+        $(this).delay(d).animate({
+            opacity: 1
+        }, {
+            step: function (n) {
+                //rotating the images on the Y axis from 360deg to 0deg
+                ry = (1 - n) * 360;
+                //translating the images from 1000px to 0px
+                tz = (1 - n) * 1000;
+                //applying the transformation
+                $(this).css("transform", "rotateY(" + ry + "deg) translateZ(" + tz + "px)");
+            },
+            duration: 3000,
+            //some easing fun. Comes from the jquery easing plugin.
+            easing: 'easeOutQuint',
+        })
+    })
+}
